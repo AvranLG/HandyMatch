1975ffd4770ac00ea6390fd4dfc3aab28a8503ee
Alvaroytv <aalvaro695@gmail.com>
2025-03-25 08:25:02 -0600

    bug de Retrieve_Password arreglado

--- app/src/main/java/com/example/app/Retrieve_Password.java
+++ app/src/main/java/com/example/app/Retrieve_Password.java
@@ -1,7 +1,10 @@
 package com.example.app;
 
+import android.graphics.Color;
 import android.os.Bundle;
-
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.View;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -20,5 +23,14 @@ public class Retrieve_Password extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Cambiar el color de la barra de estado
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(Color.parseColor("#FFFFFF")); // Cambia el color
+    }
+    // Regresar
+    public void goBack(View view) {
+        finish(); // Cierra la Activity actual
     }
 }
