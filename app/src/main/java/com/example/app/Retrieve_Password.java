87400c2a2044da0c7fd4e5c29c396d71723c3951
OmarAG <omar.avina.2003@gmail.com>
2025-05-03 00:50:19 -0600

    recuperar contraseña corregido

--- app/src/main/java/com/example/app/Retrieve_Password.java
+++ app/src/main/java/com/example/app/Retrieve_Password.java
@@ -1,36 +1,61 @@
 package com.example.app;
-
-import android.graphics.Color;
-import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
-import android.view.View;
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 public class Retrieve_Password extends AppCompatActivity {
+    TextInputEditText textEmail_text;
+    FirebaseAuth auth;
+    Button resetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_retrieve_password);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
-        // Cambiar el color de la barra de estado
-        Window window = getWindow();
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        window.setStatusBarColor(Color.parseColor("#FFFFFF")); // Cambia el color
+        textEmail_text = (TextInputEditText) findViewById(R.id.emailRecuperarText);
+        resetButton = (Button) findViewById(R.id.btnRecuperar);
+        auth = FirebaseAuth.getInstance();
     }
-    // Regresar
-    public void goBack(View view) {
-        finish(); // Cierra la Activity actual
+
+    private boolean CorreoValido(String correo) {
+        if (!Patterns.EMAIL_ADDRESS.matcher(correo).matches()) {
+            textEmail_text.setError("Correo electrónico inválido");
+            return false;
+        } else {
+            textEmail_text.setError(null);
+        }
+        return true;
+    }
+
+    public void resetPwd(View v)
+    {
+        String email = textEmail_text.getText().toString().trim();
+        if (CorreoValido(email)) {
+            auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(Retrieve_Password.this,
+                                "Te hemos enviado instrucciones para " +
+                                        "restablecer tu contraseña",
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(Retrieve_Password.this,
+                                "Error al enviar correo electrónico " +
+                                        "para reestablecer contraseña",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
